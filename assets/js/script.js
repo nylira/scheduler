0bfd6a2f3a20ef686d245bbd0d30360c433f220b
Peng Zhong <peng@nylira.com>
2012-02-26 04:41:25 +0900

    version one complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,12 @@
-/* Author:
+var current_date = new Date();
 
-*/
-
-
-$(document).ready(function(){
-	
-	// Run Matt Kersley's jQuery Responsive menu plugin (see plugins.js)
-	if ($.fn.mobileMenu) {
-		$('ol#id').mobileMenu({
-			switchWidth: 768,                   // width (in px to switch at)
-			topOptionText: 'Choose a page',     // first option text
-			indentString: '&nbsp;&nbsp;&nbsp;'  // string for indenting nested items
-		});
-	}
-
-	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
-	if ($.fn.placeholder) {
-		$('input, textarea').placeholder();		
-	}
+$(".datetime").AnyTime_picker({
+  format: "%M %e at %l:%i %p",
+  earliest: new Date(),
+  latest: new Date(new Date().setMonth(current_date.getMonth()+1))
 });
 
-
-
-
-
-
-
+$("#phases").cycle({
+  timeout: 0,
+  next: ".btn-bgcheck"
+});
