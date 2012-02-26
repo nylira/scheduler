93a2aef05ee6806d562f4c7a38061741bc8bd8fa
Peng Zhong <peng@nylira.com>
2012-02-26 17:34:10 +0900

    added toggleable button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,10 @@ $("#phases").cycle({
   timeout: 0,
   next: ".btn-bgcheck"
 });
+
+
+$("#credit-score").keypress(function () {
+  if ($("#credit-score").val().length > 0) {
+    $("#schedule-interview").removeClass("disabled").addClass("btn-primary")
+  }
+});
